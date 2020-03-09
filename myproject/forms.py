afcd55cbeb472f0177cb63a18b7484936563a834
Tseng <tzeng13579@gmail.com>
2020-03-09 15:43:34 -0700

    update front end

--- myproject/forms.py
+++ myproject/forms.py
@@ -8,8 +8,8 @@ from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(1, 20)])
+                           validators=[DataRequired(), Length(1, 20)], render_kw={"placeholder": "User Name/ Work Number"})
     password = PasswordField('Password',
-                             validators=[DataRequired(), Length(6, 20)])
+                             validators=[DataRequired(), Length(6, 20)], render_kw={"placeholder": "Password"})
     # remember = BooleanField('Remember me', default=False)
-    submit = SubmitField('Login')
+    submit = SubmitField('Log in')
